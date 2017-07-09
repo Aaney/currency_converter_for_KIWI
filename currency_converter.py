a789ad39d346e2777c135bd92dda8ac7bdd91f17
Ane <Karfiol91@seznam.cz>
2017-07-09 16:18:26 +0200

    web API output is now json!
    Finally the web API displays the output as "pretty" json, not anymore as a string on one line.

--- currency_converter.py
+++ currency_converter.py
@@ -129,7 +129,7 @@ class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             # Send the html message
             k = parse_qs(self.path[2:])
